0546ecb135f23fdba0e390568afd7effbb692d3c
nekonyanneko <morita.d.shun@gmail.com>
2017-03-17 23:52:07 +0900

    重回帰分析のコードまでを追加。

---
+++ sample_code/model_sel_aic.R
@@ -0,0 +1,22 @@
+source("clear.R")
+
+## シミュレーションデータの作成
+set.seed(0)
+N <- 100                        # サンプルサイズ
+Intercept <- 5                  # 切片
+B1 <- 10                        # 係数1
+B2 <- 5                         # 係数2
+x1 <- sort(rnorm(N, sd=2))      # 説明変数1
+x2 <- rnorm(N, sd=2)            # 説明変数2
+e <- rnorm(n=N, sd=3)           # 誤差
+# データ作成
+y <- Intercept + B1*x1 + B2*x2 + e
+
+## 重回帰モデルの作成
+# Intercept + B1*x1 + e のモデル生成
+model1 <- lm(y ~ x1)
+# Intercept + B1*x1 + B2*x2 + e
+model2 <- lm(y ~ x1 + x2)
+# Intercept + B1*x1 + B2*x2 + B1*B2*x3 + e
+model3 <- lm(y ~ x1*x2)
+
