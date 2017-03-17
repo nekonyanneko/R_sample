ce300717f35c6d0f78fa0e9aabe280a22131b6f0
nekonyanneko <morita.d.shun@gmail.com>
2017-03-18 00:39:58 +0900

    NNの実行コードの追加。#6

--- sample_code/nn.R
+++ sample_code/nn.R
@@ -7,3 +7,7 @@ area1 <- area/10000  # 岩の面積
 peri1 <- peri/10000  # 岩の周辺長
 rock1 <- data.frame(perm, area=area1, peri=peri1, shape)
 
+# NNの学習とモデル作成
+rock.nn <- 
+  nnet(log(perm) ~ area + peri + shape, rock1,
+       size=3, decay=1e-3, linout=T, skip=T, maxit=1000, Hess=T)
