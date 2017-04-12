6cf8c08c1e542557dfaa47f70db11a6986afcf53
nekonyanneko <morita.d.shun@gmail.com>
2017-04-12 21:56:27 +0900

    まずは利用方法の流れを掴む#14

--- sample_code/mlr.R
+++ sample_code/mlr.R
@@ -1,2 +1,20 @@
 source("clear.R")
 
+library(dplyr)
+library(mlr)
+data(iris)
+
+#################
+# 利用の流れ
+#################
+# タスクの定義
+task = makeClassifTask(id = "tutorial", data = iris, target = "Species")
+# 学習器の定義
+lrn = makeLearner("classif.lda")
+# リサンプリング法の定義
+rdesc = makeResampleDesc(method = "CV", stratify = TRUE)
+# 実行
+r = resample(learner = lrn, task = task, resampling = rdesc, show.info = FALSE)
+# 誤分類率
+r$aggr %>% print()
+
