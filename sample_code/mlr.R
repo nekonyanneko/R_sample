a0db9b6875849603d19a432df699c703141ff3d2
nekonyanneko <morita.d.shun@gmail.com>
2017-04-12 22:40:53 +0900

    タスクに登録したデータの修正・変更を行うコードの追加。#14

--- sample_code/mlr.R
+++ sample_code/mlr.R
@@ -50,10 +50,34 @@ cluster.task %>% print()
 getTaskDesc(regr.task) %>% print()
 # タスクで使うデータ
 str(getTaskData(classif.task))
+# データの件数
+getTaskSize(classif.task) %>% print()
+# 特徴量の数
+getTaskNFeats(cluster.task) %>% print()
+# 特徴量の名前
+getTaskFeatureNames(cluster.task) %>% print()
+# 目的変数の名前
+getTaskTargetNames(classif.task) %>% print()
+# 目的変数の値
+head(getTaskTargets(regr.task)) %>% print()
+# コスト行列
+getTaskCosts(classif.task) %>% print()
+getTaskFormula(classif.task) %>% print()
 
-
-
-
+#################
+# タスクの修正や変更
+#################
+# データの一部だけ使う
+subsetTask(cluster.task, subset = 4:17)
+# 定数値の特徴量を捨てる
+removeConstantFeatures(cluster.task)
+# 定数値の特徴量を捨てる
+removeConstantFeatures(cluster.task)
+# 変数の正規化
+## （method="range"は値が区間[0, 1]に収まるようスケールする）
+summary(getTaskData(normalizeFeatures(cluster.task, method = "range")))
+## （method="standardize"は平均0，分散1に正規化する）
+summary(getTaskData(normalizeFeatures(regr.task, method = "standardize")))
 
 
 
