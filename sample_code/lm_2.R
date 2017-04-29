ba11fc0702c149c2b4c89fc668190252224b26de
nekonyanneko <morita.d.shun@gmail.com>
2017-04-29 16:36:06 +0900

    勉強会後の修正

--- sample_code/lm_2.R
+++ sample_code/lm_2.R
@@ -15,7 +15,10 @@ y <- Intercept + B1*x1 + B2*x2 + e
 ## 重回帰モデルの作成
 # Intercept + B1*x1 + e のモデル生成
 model1 <- lm(y ~ x1)
+print(model1)
 # Intercept + B1*x1 + B2*x2 + e
 model2 <- lm(y ~ x1 + x2)
+print(model2)
 # Intercept + B1*x1 + B2*x2 + B3*x1*x2 + e
 model3 <- lm(y ~ x1*x2)
+print(model3)
